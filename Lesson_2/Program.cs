adfc7e4a768707b57af848d04b58bc64dd8eb1ae
Artem Viatkin <newyorkbox47@gmail.com>
2022-11-04 12:23:05 +0200

    commit 3 4.11.12 Artem

--- Lesson_2/Program.cs
+++ Lesson_2/Program.cs
@@ -2,6 +2,8 @@
 using System.Text;
 
 BasicStringFunctionality();
+StringConcatenation();
+EscapeChars();
 
 static void BasicStringFunctionality()
 {
@@ -19,5 +21,27 @@ static void BasicStringFunctionality()
     Console.WriteLine("firstName contains the letter y?: {0}", firstName.Contains("y"));
     // вывод firstName после замены 
     Console.WriteLine("New first name: {0}", firstName.Replace("em",""));
+    Console.WriteLine();
+}
+
+static void StringConcatenation() //конкатенация строк
+{
+    Console.WriteLine("=>String concatenation:");
+    string s1 = "Programming the";
+    string s2 = "PsychoDrill (PTP)";
+    string s3 = String.Concat(s1, s2);
+    Console.WriteLine(s3);
+    Console.WriteLine();
+}
+
+static void EscapeChars() //использование управляющих последовательностей
+{
+    Console.WriteLine("Escape characters:\a");
+    string strWithTabs = "Model\tColor\tSpeed\tPet Name\a";
+    Console.WriteLine(strWithTabs);
+    Console.WriteLine("Everyone loves \"Hello World\"\a");
+    Console.WriteLine("C:\\MyApp\\bin\\Debug\a");
+    // Добавить 4 пустых строки и снова выдать звуковой сигнал
+    Console.WriteLine("All finished. \n\n\n\a");
     Console.WriteLine();
 }
